7f758512005a2a92905739d3f1443e652bc50f3f
Anup Prajapati <anup9621prajapati@gmail.com>
2026-05-16 14:32:21 +0530

    changed port to 8080 for railway

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.get('/', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`🚀 Server running on ${PORT}`);
